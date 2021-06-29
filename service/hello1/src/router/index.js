1ac4e308887f2234d8fbeba5254fddbfd91b9e95
汪洋 <wyang@shgbit.com>
2021-06-29 14:39:42 +0800

    refactor service

---
+++ service/hello1/src/router/index.js
@@ -0,0 +1,83 @@
+const Router = require("koa-router");
+const { Test } = require("../model");
+const router = new Router({
+  prefix: "/api",
+});
+const axios = require("axios");
+const config = require("config");
+
+router.get("/", async (ctx) => {
+  ctx.body = "get request";
+  ctx.status = 200;
+});
+
+router.get("/hello1", async (ctx) => {
+  const serviceName = "hello1";
+  console.log(
+    `request => ${config.hello2Url}${config.hello2Uri.hello2}?name=${serviceName}`
+  );
+
+  let result = await axios({
+    method: "get",
+    url: `${config.hello2ServiceName}${config.hello2Uri.hello2}?name=${serviceName}`,
+    headers: {},
+  });
+
+  ctx.body = result.data;
+  ctx.status = 200;
+});
+
+router.post("/", async (ctx) => {
+  console.log(ctx.request.body);
+  ctx.body = "post request";
+  ctx.status = 200;
+});
+
+router.post("/test", async (ctx) => {
+  let { key, value } = ctx.request.body;
+  let result = await Test.create({ key, value });
+  ctx.body = result;
+  ctx.status = 201;
+});
+
+router.delete("/test/:id", async (ctx) => {
+  await Test.deleteOne({ _id: ctx.params.id });
+  ctx.body = "delete success";
+  ctx.status = 200;
+});
+
+router.patch("/test/:id", async (ctx) => {
+  let newData = ctx.request.body;
+  let result = await Test.findByIdAndUpdate(ctx.params.id, newData, {
+    new: true,
+  });
+  ctx.body = result;
+  ctx.status = 201;
+});
+
+router.get("/test", async (ctx) => {
+  let page = parseInt(ctx.query.page);
+  let size = parseInt(ctx.query.size);
+  let key = ctx.query.q;
+
+  let condition = {};
+  if (key) {
+    const reg = new RegExp(key, "i");
+    condition.key = { $regex: reg };
+  }
+
+  let result = await Test.find(condition)
+    .sort({ value: 1 })
+    .skip((page - 1) * size)
+    .limit(size);
+  ctx.body = result;
+  ctx.status = 200;
+});
+
+router.get("/test/:id", async (ctx) => {
+  let result = await Test.findById(ctx.params.id);
+  ctx.body = result;
+  ctx.status = 200;
+});
+
+module.exports = router;
---
+++ service/hello2/index.js
@@ -0,0 +1,24 @@
+const Koa = require("koa");
+const router = require("./src/router");
+const app = new Koa();
+const bodyParser = require("koa-bodyparser");
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/mock',{
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// })
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// mongoose.connection.on('connected', () => {
+//   console.log(`Mongoose default connection to: localhost:27017`);
+// });
+
+app.listen(3001, () => {
+  console.log("server listen on port 3001");
+});
